263602c000c490e97f33662fbb61a770ff2b1a04
Sameer0223 <133659852+Sameer0223@users.noreply.github.com>
2024-07-18 01:03:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NSS_CLUB/', // base should be the repository name
+  // base should be the repository name
 });
