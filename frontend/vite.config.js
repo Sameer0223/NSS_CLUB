338c5fbea6abc149b3c4de3d00400e1aa7fcb7ac
Sameer <learngoodforend@gmail.com>
2024-07-18 08:45:10 +0530

    Prepare for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
+
 
 });
 
   // base should be the repository name
 
 
-=======
-<<<<<<< HEAD
-});
->>>>>>> 3e6bf1c19a651c38a5590b022e48eefe9bbb216b
+
+
+
