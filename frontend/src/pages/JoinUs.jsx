c380e473ea3f57b6234781f8f02d34d7506768ae
Sameer0223 <133659852+Sameer0223@users.noreply.github.com>
2024-08-31 09:41:19 +0530

    Update JoinUs.jsx

--- frontend/src/pages/JoinUs.jsx
+++ frontend/src/pages/JoinUs.jsx
@@ -6,7 +6,7 @@ const JoinUs = () => {
     <>
       <div className="py-10 bg-gray-100">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h2 className="text-4xl font-bold text-center mb-8">Join NSS Club</h2>
+          <h2 className="text-4xl font-bold text-center mb-8">Join NSS Organization</h2>
           <div className="bg-white rounded-lg shadow-md p-6 mb-8">
             <div className="flex flex-col md:flex-row items-center">
               <img 
@@ -15,7 +15,7 @@ const JoinUs = () => {
                 className="w-full md:w-1/2 rounded-lg mb-6 md:mb-0 md:mr-6"
               />
               <div>
-                <h3 className="text-2xl font-semibold mb-4">About NSS Club</h3>
+                <h3 className="text-2xl font-semibold mb-4">About NSS Organization</h3>
                 <p className="text-gray-700 mb-4 text-justify">
                 National Service Scheme is an initiative taken by the Indian government to engage students in nation building activities. The program was launched in 1969 with the primary objective of developing the personality and character of the youth through voluntary community service. ‘Education through Service’ is the purpose of the NSS. The ideological orientation of the NSS is inspired by the ideals of Mahatma Gandhi. NSS has been a major part of the Indian education system. It encourages students to take part in social service activities.
                 </p>
@@ -24,7 +24,7 @@ const JoinUs = () => {
                   href="https://docs.google.com/forms/d/e/1FAIpQLSe7RJfHjHBWN2gnDYFJaOU2XztO-aSifj84m9X1j20TTCHPZg/viewform?usp=sf_link" 
                   className="inline-block bg-blue-500 text-white text-center rounded-lg py-2 px-4 hover:bg-blue-700 transition duration-300 text-sm"
                 >
-                  Register for NSS Club Membership
+                  Register for NSS Organization Membership
                 </a>
               </div>
             </div>
